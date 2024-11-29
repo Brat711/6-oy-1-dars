c7e1b2357d557085d7cb9862766ad36b42d575af
saidmuhamedovsaidakbar27@gmail.com <Brat711>
2024-11-29 21:06:12 +0500

    alooo

---
+++ main.js
@@ -0,0 +1,12 @@
+// function bot(){
+//     document.body.style.backgroundColor = "#fb982f"
+// }
+// function bot2(){
+//     document.body.style.backgroundColor = "black"
+// }
+function bot2(){
+    document.body.classList.add("dark")
+}
+function bot(){
+    document.body.classList.remove("dark")
+}
